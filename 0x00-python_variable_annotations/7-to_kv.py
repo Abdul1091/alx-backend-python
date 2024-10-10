8a35c659b9678953e912b86b0bc84ad1967a288e
Abdul1091 <Abdul2kur1@gmail.com>
2024-10-10 02:44:49 -0700

    Complex types - string and int/float to tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Complex types - string and int/float to tuple """
+
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''Computes the sum of a list of integers and floating-point numbers.
+    '''
+    return float(sum(mxd_lst))
