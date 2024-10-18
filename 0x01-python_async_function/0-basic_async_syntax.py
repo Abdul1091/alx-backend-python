665238b9bdb2c0cf6a5245928690a11cf918f7c7
Abdul1091 <Abdul2kur1@gmail.com>
2024-10-18 19:58:39 +0000

    Fix random delay generation to include max_delay in wait_random

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,8 +6,8 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    '''Waits for a random number of seconds.
+    '''Waits for a random number of seconds between 0 and max_delay (inclusive).
     '''
-    wait_time = random.random() * max_delay
+    wait_time = random.uniform(0, max_delay)
     await asyncio.sleep(wait_time)
     return wait_time
